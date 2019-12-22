4cd5797089a72437661c194a6705c00adf4286f1
iromashko <iromashko@me.com>
2019-12-22 10:28:16 +0300

    test creating tasks

--- cypress/integration/examples/tasks.js
+++ cypress/integration/examples/tasks.js
@@ -1,5 +1,37 @@
+const faker = require('faker');
+
 describe('Tasks API', () => {
-  it('Create Task', () => {});
-  it('Get Task By ID', () => {});
-  it('List All Tasks', () => {});
+  it('Create Task', () => {
+    for (let i = 0; i < 3; i++) {
+      const task = {
+        title: faker.commerce.product(),
+        description: faker.commerce.productName(),
+      };
+      cy.request('POST', 'tasks', task).then(response => {
+        expect(response.body).to.have.any.keys(
+          'id',
+          'title',
+          'status',
+          'description',
+        );
+      });
+    }
+  });
+
+  it('List All Tasks', () => {
+    cy.request('GET', '/tasks').then(response => {
+      expect(response.body).to.have.lengthOf(3);
+    });
+  });
+
+  it('Delete Tasks', () => {
+    cy.request('GET', '/tasks')
+      .its('body')
+      .each(task => {
+        const taskId = task.id;
+        cy.request('DELETE', `/tasks/${taskId}`).then(response => {
+          cy.log(response);
+        });
+      });
+  });
 });
